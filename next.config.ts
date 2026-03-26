e72ff448d41b5e942e39f52343bfaef3bce3364c
thenicolaf <d.nicolaevv@gmail.com>
2026-03-26 22:34:34 +0200

    added backup and change next config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cgqmggehyrriorehbcgo.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "tcucwizupntwctwhumzk.supabase.co",
+      },
     ],
   },
 };
